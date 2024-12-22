64ab30ca5ab83b86dce135bc8e4e72ae71f9c5cf
ice breaker <1324318532@qq.com>
2024-12-23 00:51:10 +0800

    c

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,12 +10,14 @@ async function getUser() {
   const { data } = await axios.get('https://example.com/user')
   user.value = data
 }
+
+const envJson = import.meta.env
 </script>
 
 <template>
   <div class="container mx-auto">
     <h1 class="text-3xl font-bold underline">
-      Hello world!
+      {{ envJson }}
     </h1>
     <div v-if="user">
       {{ user }}
