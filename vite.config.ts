12c002a794dbf5550751fdd1f1db1db5db829b2a
ice breaker <1324318532@qq.com>
2024-12-23 23:30:47 +0800

    ss

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,24 @@ export default defineConfig({
         globIgnores: ['**\/node_modules\/**\/*', 'mockServiceWorker.js'],
 
       },
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
       devOptions: {
         enabled: true,
         type: 'module',
